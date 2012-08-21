2c1a28ab3b4487d5480181cf5fdf672540106fa1
sparkweb <sparkweb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-21 21:12:08 +0000

    1.6 - file version updated
    git-svn-id: https://plugins.svn.wordpress.org/custom-field-bulk-editor/tags/1.6@588585 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- custom-field-bulk-editor.php
+++ custom-field-bulk-editor.php
@@ -5,7 +5,7 @@ Plugin Name: Custom Field Bulk Editor
 Plugin URI: http://wordpress.org/extend/plugins/custom-field-bulk-editor/
 Description: Allows you to edit your custom fields in bulk. Works with custom post types.
 Author: SparkWeb Interactive, Inc.
-Version: 1.5
+Version: 1.6
 Author URI: http://www.soapboxdave.com/
 
 **************************************************************************
